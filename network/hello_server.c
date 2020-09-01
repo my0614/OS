c33a4a51800098baa668d903e52658ed220ddad8
my0614 <aqi222@naver.com>
2020-09-01 10:16:58 +0900

    ADD :소켓통신 server

--- network/hello_server.c
+++ network/hello_server.c
@@ -7,42 +7,51 @@
 
 void error_handling(char *message);
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 
-	int sock;
+	
+	int serv_sock;
+	int clnt_sock;
+	char txt[20];
 	struct sockaddr_in serv_addr;
-	char message[30];
-	int str_len;
+	struct sockaddr_in clnt_addr;
+	socklen_t clnt_addr_size;
+	char message[20];
 
-	if(argc!=3)
+	strcpy(message,txt);
+
+	if(argc!=2)
 	{
-		printf("Usage : %s <ip>  <port>\n", argv[0]);
+		printf("Usage : %s <port> \n",argv[0]);
 		exit(1);
 	}
-	
-	sock = socket(PF_INET, SOCK_STREAM, 0);
-	if(sock == -1)
+
+	serv_sock = socket(PF_INET, SOCK_STREAM,0);
+	if(serv_sock == -1)
 		error_handling("socket() error");
+	memset(&serv_addr,0,sizeof(serv_addr));
+	serv_addr.sin_addr.s_addr= htonl(INADDR_ANY);
+	serv_addr.sin_port=htons(atoi(argv[1]));
 
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port = htons(atoi(argv[2]));
-	printf("success");
+	if(bind(serv_sock,(struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
+		error_handling("listen() error");
+	if(listen(serv_sock,5) == -1)
+		error_handling("listen() error");
 	
-	if(connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
-		error_handling("connect() error!");
-	
-	str_len = read(sock, message, sizeof(message)-1);
-	if(str_len == -1)
-		error_handling("read() error!");
-
-	printf("Message from server : %s \n", message);
-	close(sock);
+	clnt_addr_size = sizeof(clnt_addr);
+	while(1)
+	{
+		gets(txt);
+	clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+		if(clnt_sock == -1)
+			error_handling("accept() error");
+	write(clnt_sock, message, sizeof(message));
+	close(clnt_sock);
+	close(serv_sock);
 	return 0;
-
-
+	
+	}
 }
 
 void error_handling(char *message)
