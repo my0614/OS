dc5d5f33f873c741bb2b66c0ba45d8c43a24d3ee
my0614 <aqi222@naver.com>
2020-09-15 14:40:08 +0900

    ADD : file 보내기 server

---
+++ network/file_server.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+
+	
+	int serv_sock;
+	int clnt_sock;
+	char txt[20];
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in clnt_addr;
+	socklen_t clnt_addr_size;
+	char message[100];
+	int str_len;
+	FILE *fp;
+	char buffer;
+
+
+	if(argc!=2)
+	{
+		printf("Usage : %s <port> \n",argv[0]);
+		exit(1);
+	}
+
+	serv_sock = socket(PF_INET, SOCK_STREAM,0);
+	if(serv_sock == -1)
+		error_handling("socket() error");
+	memset(&serv_addr,0,sizeof(serv_addr));
+	serv_addr.sin_addr.s_addr= htonl(INADDR_ANY);
+	serv_addr.sin_port=htons(atoi(argv[1]));
+
+	if(bind(serv_sock,(struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
+		error_handling("listen() error");
+	if(listen(serv_sock,5) == -1)
+		error_handling("listen() error");
+	
+	clnt_addr_size = sizeof(clnt_addr);
+
+	
+	clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+		if(clnt_sock == -1)
+			error_handling("accept() error");
+	while((str_len = read(clnt_sock, message,BUFSIZ)) !=0) {
+	write(clnt_sock, message, sizeof(str_len));
+
+	fp = fopen("txt.txt",  "r");
+		if(fp == NULL)
+		{
+			printf("fp is file error\n");
+		}
+		fscanf(fp, "%s", message); // text
+		write(clnt_sock, message, sizeof(message));
+		printf("result : %s\n",message);
+	
+	}
+	fclose(fp);
+	free(buffer);
+	close(clnt_sock);
+	close(serv_sock);
+	return 0;
+	
+	
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n',stderr);
+	exit(1);
+}
+
