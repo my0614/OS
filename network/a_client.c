6d69b56cc62f267b5d8da569a13bbb5101e24273
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2020-11-01 17:29:10 +0900

    Update and rename a_client.cpp to a_client.c
    get명령어 구현

--- network/a_client.c
+++ network/a_client.c
@@ -11,7 +11,7 @@
 #include <sys/socket.h>
 
 #include <dirent.h> 
-
+#include <fcntl.h>
  
 
 void error_handling(char *message);
@@ -68,16 +68,17 @@ int main(int argc, char * argv[])
 
 		error_handling("connect() error");
 
-	//loop
+	
 	while(1) {
+//	fgets(message,BUFSIZ,stdin);
 	str_len = read(sock, message, sizeof(message)-1);
 
 	if(str_len == -1)
 
 		error_handling("read() error");
 
-	
-	if(!strcmp(message,"ls")) {
+//	printf("message = %s\n\n\n",message);	
+	if(strcmp(message,"ls")==0) {
 	printf("Message from server : %s\n", message);
 	
        		
@@ -91,15 +92,68 @@ int main(int argc, char * argv[])
 		{
 			printf("%s\n", entry->d_name);
 		}
+		
 		free(cwd);
-		closedir(dir);
-		close(sock);
-		}
+		
 	}
-	return 0;	
 
+	char cd_name[30]; // filename size 30
+	if(message[0] == 'c' && message[1] == 'd')
+	{
+		int index = 3;
+		while(message[index] != NULL)
+		{
+			strcat(cd_name, message[index]);
+			index++;
+		
+		}
+		chdir(cd_name);
+		
+		printf("success");
+		//message = NULL;
+	
+		
+		
+	}
+	if(message[0] =='g'&& message[1]=='e'&& message[2] == 't')
+	{
+		char file_name[30], file_name2[30] = {0,};
+		int len = strlen(message);
+		printf("len = %d\n",len);
+		printf("message = %s", message);
+		int index =4;
+		FILE *fp;
+		char buffer[100];
+		memcpy(file_name,message,strlen(message)+1);
+		printf("file_name is =%s\n",file_name);	
+		for(int i = index; i<len+1;i++)
+		{
+			sprintf(file_name, "%c", message[i]);
+			
+			strcat(file_name2, file_name);
+		}
+		//printf("%s", message);		
+			
+		printf("file name=%s\n", file_name2);
+		fp = open(file_name2, O_WRONLY);
+		if(fp == -1)
+		{
+			error_handling("open() error!");
+		}
+		if(read(fp, buffer, sizeof(buffer)) == -1)
+		{
+
+			error_handling("read() error!");
+		}
+		
+		printf("text is = %s", buffer);
+		
+	}
+	
+	//return 0;	
+	
 }
-
+}
  
 
 void error_handling(char *message)
